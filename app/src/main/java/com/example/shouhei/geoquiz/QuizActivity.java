c2955630d77eac31a6f031fe135f768af8ef57ec
Shouhei.Mizuno <32non23@gmail.com>
2018-05-25 10:50:57 -0700

    fix a bug of rotation and challenge preventing repeat answers

--- app/src/main/java/com/example/shouhei/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/shouhei/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.shouhei.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,11 +11,15 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+  private static final String TAG = "QuizActivity";
+  private static final String KEY_INDEX = "index";
+  private static final String KEY_IS_ANSWERED = "isAnswered";
   private Button mTrueButton;
   private Button mFalseButton;
   private ImageButton mNextButton;
   private ImageButton mPrevButton;
   private TextView mQuestionTextView;
+  private boolean mIsAnswered;
 
   private Question[] mQuestionBank =
       new Question[] {
@@ -31,21 +36,30 @@ public class QuizActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
     setContentView(R.layout.activity_quiz);
+
+    mTrueButton = findViewById(R.id.true_button);
+    mFalseButton = findViewById(R.id.false_button);
+
+    if (savedInstanceState != null) {
+      mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+      mIsAnswered = savedInstanceState.getBoolean(KEY_IS_ANSWERED);
+    }
 
     // This is connect
     mQuestionTextView = findViewById(R.id.questionTextView);
     int question = mQuestionBank[mCurrentIndex].getmTextResId();
     mQuestionTextView.setText(question);
 
-    mTrueButton = findViewById(R.id.true_button);
-    mFalseButton = findViewById(R.id.false_button);
-
     mTrueButton.setOnClickListener(
         new View.OnClickListener() {
           @Override
           public void onClick(View v) {
             checkAnswer(true);
+
+            mIsAnswered = true;
+            updateAnswerButton();
           }
         });
 
@@ -55,6 +69,9 @@ public class QuizActivity extends AppCompatActivity {
           @Override
           public void onClick(View v) {
             checkAnswer(false);
+
+            mIsAnswered = true;
+            updateAnswerButton();
           }
         });
 
@@ -65,6 +82,8 @@ public class QuizActivity extends AppCompatActivity {
           public void onClick(View v) {
             mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
             updateQuestion();
+            mIsAnswered = false;
+            updateAnswerButton();
           }
         });
 
@@ -75,6 +94,8 @@ public class QuizActivity extends AppCompatActivity {
           public void onClick(View v) {
             mCurrentIndex = (Math.abs(mCurrentIndex - 1)) % mQuestionBank.length;
             updateQuestion();
+            mIsAnswered = false;
+            updateAnswerButton();
           }
         });
 
@@ -89,11 +110,60 @@ public class QuizActivity extends AppCompatActivity {
         });
 
     updateQuestion();
+    updateAnswerButton();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "onSaveInstanceState");
+    savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    savedInstanceState.putBoolean(KEY_IS_ANSWERED, mIsAnswered);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
   }
 
   private void updateQuestion() {
     int question = mQuestionBank[mCurrentIndex].getmTextResId();
     mQuestionTextView.setText(question);
+  }
+
+  private void updateAnswerButton() {
+    if (mIsAnswered) {
+      mTrueButton.setEnabled(false);
+      mFalseButton.setEnabled(false);
+    } else {
+      mTrueButton.setEnabled(true);
+      mFalseButton.setEnabled(true);
+    }
   }
 
   private void checkAnswer(boolean userPressedTrue) {
