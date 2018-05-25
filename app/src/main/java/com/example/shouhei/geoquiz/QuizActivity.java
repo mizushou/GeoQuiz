25b26979a38226e49f70e947ca02f4762adfc3cb
Shouhei.Mizuno <32non23@gmail.com>
2018-05-25 16:07:49 -0700

    fix bug of prev button and implemented challenge graded quiz

--- app/src/main/java/com/example/shouhei/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/shouhei/geoquiz/QuizActivity.java
@@ -3,23 +3,27 @@ package com.example.shouhei.geoquiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
 public class QuizActivity extends AppCompatActivity {
 
   private static final String TAG = "QuizActivity";
   private static final String KEY_INDEX = "index";
-  private static final String KEY_IS_ANSWERED = "isAnswered";
+  private static final String KEY_IS_ANSWERED_ARRAY = "IsAnsweredArray";
+  private static final String KEY_ANSWER_SHEET = "AnswerSheet";
   private Button mTrueButton;
   private Button mFalseButton;
   private ImageButton mNextButton;
   private ImageButton mPrevButton;
   private TextView mQuestionTextView;
-  private boolean mIsAnswered;
 
   private Question[] mQuestionBank =
       new Question[] {
@@ -30,6 +34,9 @@ public class QuizActivity extends AppCompatActivity {
         new Question(R.string.question_americas, true),
         new Question(R.string.question_asia, true)
       };
+
+  private boolean[] mIsAnsweredArray = new boolean[mQuestionBank.length];
+  private boolean[] mAnswerSheet = new boolean[mQuestionBank.length];
 
   private int mCurrentIndex = 0;
 
@@ -44,7 +51,8 @@ public class QuizActivity extends AppCompatActivity {
 
     if (savedInstanceState != null) {
       mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
-      mIsAnswered = savedInstanceState.getBoolean(KEY_IS_ANSWERED);
+      mIsAnsweredArray = savedInstanceState.getBooleanArray(KEY_IS_ANSWERED_ARRAY);
+      mAnswerSheet = savedInstanceState.getBooleanArray(KEY_ANSWER_SHEET);
     }
 
     // This is connect
@@ -57,9 +65,10 @@ public class QuizActivity extends AppCompatActivity {
           @Override
           public void onClick(View v) {
             checkAnswer(true);
+            updateQuestionCheckArray();
 
-            mIsAnswered = true;
             updateAnswerButton();
+            checkComplete();
           }
         });
 
@@ -69,9 +78,10 @@ public class QuizActivity extends AppCompatActivity {
           @Override
           public void onClick(View v) {
             checkAnswer(false);
+            updateQuestionCheckArray();
 
-            mIsAnswered = true;
             updateAnswerButton();
+            checkComplete();
           }
         });
 
@@ -81,8 +91,8 @@ public class QuizActivity extends AppCompatActivity {
           @Override
           public void onClick(View v) {
             mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            Log.d(TAG, "Go to next " + String.valueOf(mCurrentIndex));
             updateQuestion();
-            mIsAnswered = false;
             updateAnswerButton();
           }
         });
@@ -92,9 +102,22 @@ public class QuizActivity extends AppCompatActivity {
         new View.OnClickListener() {
           @Override
           public void onClick(View v) {
+            //            int tempIndex = mQuestionBank.length - mCurrentIndex;
+            //            if (tempIndex == mQuestionBank.length) {
+            //              tempIndex = 0;
+            //            }
+            //            int tempIndex2 = (tempIndex + 1) % mQuestionBank.length;
+            //            if (tempIndex2 != 0) {
+            //              mCurrentIndex = mQuestionBank.length - tempIndex2;
+            //            } else {
+            //              mCurrentIndex = tempIndex2;
+            //            }
+            if (mCurrentIndex == 0) {
+              mCurrentIndex = 6;
+            }
             mCurrentIndex = (Math.abs(mCurrentIndex - 1)) % mQuestionBank.length;
+            Log.d(TAG, "Go to prev " + String.valueOf(mCurrentIndex));
             updateQuestion();
-            mIsAnswered = false;
             updateAnswerButton();
           }
         });
@@ -136,7 +159,8 @@ public class QuizActivity extends AppCompatActivity {
     super.onSaveInstanceState(savedInstanceState);
     Log.i(TAG, "onSaveInstanceState");
     savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
-    savedInstanceState.putBoolean(KEY_IS_ANSWERED, mIsAnswered);
+    savedInstanceState.putBooleanArray(KEY_IS_ANSWERED_ARRAY, mIsAnsweredArray);
+    savedInstanceState.putBooleanArray(KEY_ANSWER_SHEET, mAnswerSheet);
   }
 
   @Override
@@ -157,7 +181,7 @@ public class QuizActivity extends AppCompatActivity {
   }
 
   private void updateAnswerButton() {
-    if (mIsAnswered) {
+    if (mIsAnsweredArray[mCurrentIndex]) {
       mTrueButton.setEnabled(false);
       mFalseButton.setEnabled(false);
     } else {
@@ -172,11 +196,55 @@ public class QuizActivity extends AppCompatActivity {
     int messageResId = 0;
 
     if (userPressedTrue == answerIsTrue) {
+      mAnswerSheet[mCurrentIndex] = true;
       messageResId = R.string.correct_toast;
     } else {
       messageResId = R.string.incorrect_toast;
     }
 
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+  }
+
+  private void updateQuestionCheckArray() {
+    mIsAnsweredArray[mCurrentIndex] = true;
+  }
+
+  private void checkComplete() {
+
+    if (isCompleted()) {
+
+      String message = getString(R.string.complete_toast);
+      String res = getResultAsPercentage();
+
+      Toast toast = Toast.makeText(QuizActivity.this, message + " " + res, Toast.LENGTH_SHORT);
+      toast.setGravity(Gravity.TOP, 0, 0);
+      toast.show();
+    }
+  }
+
+  private boolean isCompleted() {
+    for (boolean b : mIsAnsweredArray) {
+      if (!b) return false;
+    }
+    return true;
+  }
+
+  private int getNumOfCorrect() {
+    int cnt = 0;
+    for (boolean b : mAnswerSheet) {
+      if (b) cnt++;
+    }
+    return cnt;
+  }
+
+  private String getResultAsPercentage() {
+    BigDecimal numOfCorrect = BigDecimal.valueOf(getNumOfCorrect());
+    BigDecimal numOfQuestion = BigDecimal.valueOf(mQuestionBank.length);
+    BigDecimal res = numOfCorrect.divide(numOfQuestion, 2, BigDecimal.ROUND_HALF_UP);
+    Log.d(TAG, "Correct is " + String.valueOf(numOfCorrect));
+    Log.d(TAG, "Question is " + String.valueOf(numOfQuestion));
+    Log.d(TAG, "Result is " + String.valueOf(res));
+    NumberFormat formatPer = NumberFormat.getPercentInstance();
+    return formatPer.format(res.doubleValue());
   }
 }
